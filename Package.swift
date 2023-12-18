b571c26aeb06fdf85d3eef9518d1ab91b7d7b645
Andrey Snetkov <andrey.snetkov@wexinc.com>
2023-12-18 19:50:10 +0400

    Updated package

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Pendo",
-            url: "https://software.mobile.pendo.io/artifactory/ios-sdk-release/2.22.6.8014/pendo-ios-sdk-xcframework.2.22.6.8014.zip",
+            url: "https://sandr77.github.io/pendo-mobile-sdk/pendo-ios-sdk-xcframework.3.0.0.7964.zip",
             checksum: "f8df43f2dbd049c7b12cd6cfcd3ec1ea0d87739759966e19f57e737c4755bae3"
         ),
     ]
